95787766de5cf520c08fddf1d39110f94d8d18d2
imlystyi <vladte3c@gmail.com>
2024-04-04 01:12:03 +0300

    feat(server): create database context for session

---
+++ UrlShortener.Server/Contexts/SessionContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Server.Models.Entities;
+
+namespace UrlShortener.Server.Contexts;
+
+public class SessionContext(DbContextOptions<SessionContext> options) : DbContext(options)
+{
+    public DbSet<Session> Sessions { get; set; }
+}
