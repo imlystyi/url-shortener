1fa6dc452196291505f92bbae6e10752c0e85938
imlystyi <vladte3c@gmail.com>
2024-04-03 23:13:37 +0300

    refactor(server): rename ShortenedUrlInputDto to ShrotenedUrlCreateDto

---
+++ UrlShortener.Server/Models/Dto/ShortenedUrlCreateDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UrlShortener.Server.Models.Entities;
+
+namespace UrlShortener.Server.Models.Dto;
+
+public class ShortenedUrlCreateDto
+{
+    public required long AuthorId { get; set; }
+
+    [StringLength(2048)]
+    public required string FullUrl { get; set; }
+
+    [StringLength(32)]
+    public required string ShortUrl { get; set; }
+
+    public static explicit operator ShortenedUrl(ShortenedUrlCreateDto v) => new()
+    {
+            AuthorId = v.AuthorId,
+            FullUrl = v.FullUrl,
+            ShortUrl = v.ShortUrl,
+            Clicks = 0,
+            CreatedAt = DateTime.Now
+    };
+}
+
