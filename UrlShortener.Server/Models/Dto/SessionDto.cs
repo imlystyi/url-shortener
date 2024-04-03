ae05b527439ff422b919a5c70f5c99406aa48244
imlystyi <vladte3c@gmail.com>
2024-04-04 01:11:18 +0300

    feat(server): create database entity for session, add DTO for session

---
+++ UrlShortener.Server/Models/Dto/SessionDto.cs
@@ -0,0 +1,16 @@
+﻿using UrlShortener.Server.Models.Entities;
+
+namespace UrlShortener.Server.Models.Dto;
+
+public class SessionDto
+{
+    public Guid Token { get; set; }
+
+    public long UserId { get; set; }
+
+    public static explicit operator SessionDto(Session v) => new()
+    {
+        Token = v.Token,
+        UserId = v.UserId
+    };
+}
---
+++ UrlShortener.Server/Models/Entities/Session.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace UrlShortener.Server.Models.Entities;
+
+[Table("session")]
+[Index(nameof(Token), IsUnique = true)]
+public class Session
+{
+    [Column("id")]
+    [Key]
+    public long Id { get; set; }
+
+    [Column("token")]
+    [Required]
+    public Guid Token { get; set; }
+
+    [Column("user_id")]
+    [ForeignKey("user")]
+    [Required]
+    public long UserId { get; set; }
+
+    [Required]
+    public DateTime LastAccess { get; set; }
+
+    public User User { get; set; }
+}
