50e5e1679e9549ffe2c5a0a85953355b728777ff
imlystyi <vladte3c@gmail.com>
2024-04-04 19:43:25 +0300

    fix(client): delete .css from styleUrl in login component

--- urlshortener.client/src/app/components/login/login.component.ts
+++ urlshortener.client/src/app/components/login/login.component.ts
@@ -5,7 +5,6 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
 })
 export class LoginComponent {
   username: string = '';
